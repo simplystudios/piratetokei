0d2d90431d51bfe9d8e7ea4ea59d78a410fd13fa
simplystudios <new.anshwadhwa@gmail.com>
2023-10-27 20:12:22 +0530

    fixed

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const menubut  = document.getElementById('menu')
 const nav = document.getElementById('nav')
-let num = 0
+let num = 1
 
 function openmenu(){
     if (num == 0){
-        nav.style.display = 'inline-block';
+        nav.style.display = 'block';
         num = num + 1;
     }
     else if (num == 1){
